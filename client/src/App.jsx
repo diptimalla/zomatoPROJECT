519274d1e3321913a6d2fea73a9ba781a51e0ad5
diptimalla <pdaffy0@gmail.com>
2021-08-23 22:11:38 +0530

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,47 @@
+import { Route, Redirect } from "react-router-dom";
+
+//HOC
+import HomeLayoutHOC from "./HOC/Home.Hoc";
+import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+
+import Temp from "./Components/temp";
+
+//pages
+import Home from "./Page/Home";
 
 function App() {
-  return <div></div>
-   
-    
+  return (
+    <>
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <HomeLayoutHOC path="/:type" exact component={Home} />
+      <RestaurantLayoutHOC path="/restaurant/:id" exact component={Temp} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/overview"
+        exact
+        component={Temp}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/order-online"
+        exact
+        component={Temp}
+      />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/reviews"
+        exact
+        component={Temp}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/photos"
+        exact
+        component={Temp}
+      />
+    </>
+  );
 }
 
 export default App;
+
+
---
+++ client/src/Layout/home.layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+//Components
+import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
+
+const HomeLayout = (props) => {
+  return (
+    <>
+      <Navbar />
+      <FoodTab />
+      <div className="container mx-auto px-4 lg:px-20 ">{props.children}</div>
+    </>
+  );
+};
+
+export default HomeLayout;

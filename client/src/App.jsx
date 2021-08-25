3ebc3c49401a4760b24e77d8a232f16881cfa1ae
diptimalla <pdaffy0@gmail.com>
2021-08-25 13:54:44 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,47 +1,14 @@
-import { Route, Redirect } from "react-router-dom";
-
 //HOC
-import HomeLayoutHOC from "./HOC/Home.Hoc";
-import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
+import HomeLayoutHOC from "./HOC/home.hoc";
 
+// Component
 import Temp from "./Components/temp";
 
-//pages
-import Home from "./Page/Home";
-
 function App() {
-  return (
-    <>
-      <Route path="/" exact>
-        <Redirect to="/delivery" />
-      </Route>
-      <HomeLayoutHOC path="/:type" exact component={Home} />
-      <RestaurantLayoutHOC path="/restaurant/:id" exact component={Temp} />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/overview"
-        exact
-        component={Temp}
-      />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/order-online"
-        exact
-        component={Temp}
-      />
-      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Temp} />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/reviews"
-        exact
-        component={Temp}
-      />
-      <RestaurantLayoutHOC
-        path="/restaurant/:id/photos"
-        exact
-        component={Temp}
-      />
-    </>
-  );
+  return <>
+  <HomeLayoutHOC path="/" exact component={Temp}/>
+  </>;
+
 }
 
 export default App;
-
-
--- client/src/HOC/home.hoc.jsx
+++ client/src/HOC/home.hoc.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-//Layout
-import HomeLayout from "../Layout/Home.layout";
+// Layout
+import HomeLayout from "../Layout/home.layout";
 
 const HomeLayoutHOC = ({ component: Component, ...rest }) => {
   return (

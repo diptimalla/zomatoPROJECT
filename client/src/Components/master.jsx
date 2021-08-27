f77ade4b1b0da01acc9b1d0ba2f56e1c7450f390
diptimalla <pdaffy0@gmail.com>
2021-08-27 13:58:08 +0530

    night life

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -4,12 +4,17 @@ import { useParams } from "react-router-dom";
 //components
 import Delivery from "./Delivery";
 import Dining from "./Dining";
+import NightLife from "./NightLife";
 
 
 const Master = () => {
   const { type } = useParams();
 
-  return <div className="my-5">{type === "delivery" && <Delivery />} {type === "dining" && <Dining />}</div>;
+  return <div className="my-5">
+  {type === "delivery" && <Delivery />} 
+  {type === "dining" && <Dining />}
+  {type === "night" && <NightLife />}
+  </div>;
 };
 
 export default Master;

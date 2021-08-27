f9025cb098e7bb523846ee53fa60ee37d9ddfdb3
diptimalla <pdaffy0@gmail.com>
2021-08-27 13:45:31 +0530

    dining out

--- client/src/Components/master.jsx
+++ client/src/Components/master.jsx
@@ -3,11 +3,13 @@ import { useParams } from "react-router-dom";
 
 //components
 import Delivery from "./Delivery";
+import Dining from "./Dining";
+
 
 const Master = () => {
   const { type } = useParams();
 
-  return <div className="my-5">{type === "delivery" && <Delivery />}</div>;
+  return <div className="my-5">{type === "delivery" && <Delivery />} {type === "dining" && <Dining />}</div>;
 };
 
 export default Master;
